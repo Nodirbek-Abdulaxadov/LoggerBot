fe08a35d91dcaf4b77411b8348492b2535ffe20f
Nodirbek Abdulaxadov <nbkabdulaxadov2000@gmail.com>
2024-05-06 10:02:07 +0500

    aa

--- GlobalUsings.cs
+++ GlobalUsings.cs
@@ -2,3 +2,5 @@
 global using Telegram.Bot.Types.Enums;
 global using Telegram.Bot.Types;
 global using LoggerBot.Services;
+global using Microsoft.Extensions.Configuration;
+global using Microsoft.Extensions.DependencyInjection;
